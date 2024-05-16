4ecfdc26c8ed96bad7ca7445f07e9babb9e331b9
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-05-16 08:39:35 +0000

    [Add]コントローラービュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   namespace :admin do
     get 'order_details/update'
   end
