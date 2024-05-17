dd2f9d804e71a4c957f14499adf677d3837f4ce8
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-05-17 08:02:27 +0000

    [Add]配送先編集

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,27 @@
-class AddressesController < ApplicationController
+class Public::AddressesController < ApplicationController
   def index
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+        redirect_to public_addresses_index(@address)
+    else
+        render "edit"
+    end
   end
 
   def destroy
+  end
+
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name)
   end
 end
