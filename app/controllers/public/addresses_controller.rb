0ea3c79dea46d77182be6ef0f7dfa5d91e923e4e
EC2 Default User <ec2-user@ip-172-31-34-9.ap-northeast-1.compute.internal>
2024-05-18 03:40:38 +0000

    make flash message

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -10,6 +10,15 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to public_addresses_path, notice: "登録に成功しました"
+    else
+      @customer = current_customer
+      @address = @customer.address
+      flash.now[:alert] = "登録に失敗しました"
+      render :index
+    end
   end
 
   def update
