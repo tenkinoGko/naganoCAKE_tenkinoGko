be1d8696460531eee5dfaf0f1b1f49a053c10730
saenokami <165577235+saenokami@users.noreply.github.com>
2024-05-18 04:40:43 +0000

    [Add]新規登録・Logout

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -41,9 +41,47 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+
+
+  class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_to do |format|
+          format.html { redirect_to after_sign_up_path_for(resource) }
+        end
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_to do |format|
+          format.html { redirect_to after_inactive_sign_up_path_for(resource) }
+        end
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_to do |format|
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :phone_number, :address, :postal_code, :password, :password_confirmation])
+  end
+end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
