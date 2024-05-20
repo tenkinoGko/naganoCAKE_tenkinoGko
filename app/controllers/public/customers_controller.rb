5e98f65bdf1a055a763a9075eaeedf3b55e66883
saenokami <165577235+saenokami@users.noreply.github.com>
2024-05-20 09:23:02 +0000

    [Add]マイページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+
   def show
     @customer = current_customer
   end
@@ -28,4 +29,5 @@ class Public::CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :email)
   end
+
 end
