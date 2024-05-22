2d77119db5f73bf895017bc5aa16a59b67bdc8a6
saenokami <165577235+saenokami@users.noreply.github.com>
2024-05-22 09:09:19 +0000

    [Add]レイアウト

--- db/migrate/20240515072455_create_genres.rb
+++ db/migrate/20240515072455_create_genres.rb
@@ -1,9 +1,13 @@
 class CreateGenres < ActiveRecord::Migration[6.1]
+  
   def change
     create_table :genres do |t|
 
       t.timestamps
       t.string :name, null: false
     end
+    
+    add_column :genres, :is_active, :boolean, default: true
+    
   end
 end
