2001374f001cbba9c5183a97f549b3863e5f8ab2
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-05-23 07:21:05 +0000

    [Fix] 商品新規投稿のバリデーションとエラーメッセージ表記

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,7 +14,8 @@ class Admin::ItemsController < ApplicationController
     if @item.save
       redirect_to admin_item_path(@item)
     else
-      render :new
+      flash[:item_created_error] = "空欄を埋めてください"
+      redirect_to new_admin_item_path
     end
   end
 
@@ -28,23 +29,23 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    
+
     if @item.update(item_params)
       redirect_to admin_item_path(@item)
     else
       render :edit
     end
   end
-  
+
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
     redirect_to admin_items_path
   end
-  
+
   private
   def item_params
     params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_active)
   end
-  
+
 end
