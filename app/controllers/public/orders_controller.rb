4f8c93a9d22684cc9fc392502effe5a45ff92cbe
EC2 Default User <166229295+marukome21@users.noreply.github.com>
2024-05-24 09:08:28 +0000

    [Fix] ショッピングカート一覧レイアウト,注文情報入力が途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -65,7 +65,7 @@ class Public::OrdersController < ApplicationController
 
 
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders.order(created_at: :desc).page(params[:page]).per(8)
   end
 
   def show
