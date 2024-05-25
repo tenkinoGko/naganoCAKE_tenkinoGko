dc3257edd0269cffa98609421c3b5b71adacbad2
EC2 Default User <ec2-user@ip-172-31-33-215.ap-northeast-1.compute.internal>
2024-05-25 07:03:40 +0000

    レイアウト色調整経過

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,5 +1,6 @@
 class Public::GenresController < ApplicationController
   def show
+    @genres = Genre.all.page(params[:page]).per(8)
     @genre = Genre.find(params[:id])
     @items = @genre.items
   end
